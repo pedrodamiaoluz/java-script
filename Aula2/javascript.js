ca3a569243cc62a903a10ef0ca228258a83b925a
Pedro Damiao <121252897+pedrodamiaoluz@users.noreply.github.com>
2023-08-08 00:12:59 +0000

    Atualizar

---
+++ Aula2/javascript.js
@@ -0,0 +1,39 @@
+// console.log('Hello, world!');
+
+// alert('Hello, World!');
+
+// var nomeDaMinhavariavel = 'Cicim da pop 100';
+// console.log(nomeDaMinhavariavel)
+
+// let nomeDaMinhavariavelDois = 1000;
+// alert(nomeDaMinhavariavelDois)
+
+// const nomeDaMinhaConstante = true;
+// console.log(nomeDaMinhaConstante);
+
+//  let nome = prompt('Digite o seu nome');
+//  console.log(nome);
+
+//  let idade = prompt('informe sua idade');
+//  console.log(idade);
+
+// const verdadeiro= true;
+// console.log(verdadeiro);
+
+let nome = prompt('Digite seu Nome');
+//console.log(nome);
+
+let sobrenome = prompt('Digite seu Sobrenome:');
+//console.log(sobrenome);
+
+let idade = prompt('Digite sua Idade');
+//console.log(idade);
+
+const id = 123456789;
+
+// console.log('ola, ' + nome + ' ' + sobrenome + ', voce tem '+ idade + ' anos e seu id e ' + id);
+
+// console.log(`ola, ${nome} ${sobrenome}, voce tem ${idade}anos e seu ${id}`);
+
+alert(`ola, ${nome} ${sobrenome}, voce tem ${idade} anos e seu ID: ${id}`);
+
