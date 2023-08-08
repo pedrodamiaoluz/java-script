fd93009483775f5f74a4b96393a4ab541bc9d29f
Pedro Damiao <121252897+pedrodamiaoluz@users.noreply.github.com>
2023-08-08 00:56:09 +0000

    Atualizar

---
+++ Aula3/javascript.js
@@ -0,0 +1,13 @@
+// const minhavariavel = 10;
+// console.log(typeof minhavariavel)
+// Numero Para string
+// console.log(parseInt('10.7'));
+// console.log('String: 10.7');
+// console.log('Number:', parseFloat('10.7'));
+
+// let numerofloat = 10.979915;
+// console.log(numerofloat.toFixed(2))
+
+let numero = 69;
+let minhastring = numero.toString(16);
+console.log(minhastring);
